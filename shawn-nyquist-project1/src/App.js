d49be29b7d46424e018d4dde3b81e840f8cf8777
shawn-nyquist <shawn.nyquist@gmail.com>
2019-03-05 19:44:38 -0600

    Set up images and bootstrap

--- shawn-nyquist-project1/src/App.js
+++ shawn-nyquist-project1/src/App.js
@@ -1,4 +1,11 @@
 import React, { Component } from 'react';
+import Button from 'react-bootstrap/Button';
+//import ButtonToolbar from 'react-bootstrap/ButtonToolbar';
+import Image from 'react-bootstrap/Image';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import picture from './components/Images/mugtransparent.png'
 
 import './App.css';
 
@@ -7,9 +14,30 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          
+          Sup
         </header>
-      </div>
+        <div className="PictureRow">
+          <Container>
+          <Row>
+            <Col>
+              <Image src={ picture } rounded className="Pictures"/>
+            </Col>
+            <Col>
+              <Image src={ picture } className="Pictures"/>
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={6} md={4}>
+              <Image src={ picture } rounded className="Pictures"/>
+            </Col>
+            <Col xs={6} md={4}>
+              <Image src={ picture } thumbnail className="Pictures"/>
+            </Col>
+          </Row>
+        </Container>
+        </div>
+        
+      </div>   
     );
   }
 }
---
+++ shawn-nyquist-project1/src/components/Layout/layout.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import Image from 'react-bootstrap/Image';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
